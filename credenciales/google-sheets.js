51753f9caa79f62f9faadfcea5300d9fe9bc1dab
hamburglar00 <hamburglar002@proton.me>
2026-02-13 13:43:58 -0300

    Update google-sheets.js

--- credenciales/google-sheets.js
+++ credenciales/google-sheets.js
@@ -1,3 +1,3 @@
 export const CONFIG_SHEETS = {
-  GOOGLE_SHEETS_URL: 'https://script.google.com/macros/s/AKfycbwoAuyrXjLrD4N8QN0hyD5N2PXwXU8Km0VQCNTrqfpHZghQ--cgQuy1dTkz7WxlYmTtsw/exec'
+  GOOGLE_SHEETS_URL: 'https://script.google.com/macros/s/AKfycby0T0NGW0NDclhkLf7ssWzAV9Q-i1zmMUPsMV0RIR1wLVXI2uVsMsKv3Qn4h_jPHhLgCw/execc'
 };
